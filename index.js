fc5400480f9701f0d9e52bc2ec39941fcb5236fb
Add Chan <addison95132@gmail.com>
2014-09-13 15:32:48 -0700

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,55 @@ while(testForZero === false){
       b = a % b
       a = rBkup
     }
-    alert ("This is the gcd " + rBkup)
+    alert ("The gcd is " + rBkup)
+  }
+  
+/*case where a=0 or b=0
+*-----------------------
+*/
+  
+  else {
+    alert("That was an invalid input. Please try again.")
+    }
+}
+})
+
+$("#LCM").click(function(){
+  console.log("has been clicked.")
+/*get numbers: firstNum, secondNum
+*---------------------------------
+*/
+testForZero = false
+while(testForZero === false){
+
+  var firstNum = Number(prompt("Please enter the value of the first number."))
+  var secondNum = Number(prompt("Please enter the value of the second number."))
+
+/*set larger number to largerNum, smaller to smallerNum
+*------------------------------------------------------
+*/
+
+  if (firstNum !== 0 && secondNum!== 0) {
+     testForZero = true
+    if (firstNum >= secondNum) {
+      a = firstNum
+      b = secondNum
+    } else {
+      a = secondNum
+      b = firstNum
+    }
+    
+/*start gcd computations:
+*-------------------------
+*/
+
+    while(b !== 0) {
+      rBkup = b
+      b = a % b
+      a = rBkup
+    }
+    ans = firstNum*secondNum/rBkup
+    alert ("The LCM is " + ans)
   }
   
 /*case where a=0 or b=0
